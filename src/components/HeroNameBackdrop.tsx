f54012547184a1fde7ce89e564c23a53b9ded73d
AWeeHub <weealvin124@gmail.com>
2026-07-01 13:00:57 -0400

    Add HeroNameBackdrop component

---
+++ src/components/HeroNameBackdrop.tsx
@@ -0,0 +1,10 @@
+export function HeroNameBackdrop() {
+  return (
+    <p
+      aria-hidden="true"
+      className="pointer-events-none absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 select-none whitespace-nowrap text-center font-display text-[18vw] leading-none text-white/5"
+    >
+      ALVIN WEE
+    </p>
+  );
+}
